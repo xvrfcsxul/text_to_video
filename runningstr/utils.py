520d71124aadcc2b039563681004547a642299fe
Andrey Yurlov <76644125+xvrfcsxul@users.noreply.github.com>
2024-07-09 10:23:19 +0300

    Update utils.py

--- runningstr/utils.py
+++ runningstr/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def create_video(message:str):
     width, height = 100, 100
@@ -12,9 +13,13 @@ def create_video(message:str):
     font_thickness = 2
     font_color = (255, 255, 255)
 
+    text_size, _ = cv2.getTextSize(message, font, font_scale, font_thickness)
+    total_distance = width + text_size[0]
+    speed = 1 + total_distance / 72.0
+
     for t in range(72):
         frame.fill(0)
-        x -= 2
+        x -= math.floor(speed)
         cv2.putText(frame, message, (x, y), font, font_scale, font_color, font_thickness)
         out.write(frame)
 
